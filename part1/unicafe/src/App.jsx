3a34e7ebf51d19b47a15ae06e2529421e4b27152
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2025-04-24 19:07:07 +0200

    1.7: unicafe, step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,12 +5,16 @@ const Title = ({ text }) => <h3 >{text}</h3>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Display = ({ goodValue, neutralValue, badValue }) => {
+const Display = ({ goodValue, neutralValue, badValue, total, average, positive }) => {
+  console.log(average)
   return (
     <ul>
       <li>{goodValue}</li>
       <li>{neutralValue}</li>
       <li>{badValue}</li>
+      <li>{total}</li>
+      <li>{average}</li>
+      <li>{positive}</li>
     </ul>
   )
 }
@@ -20,24 +24,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
 
   const hadlerSetGood = () => {
     const counter = good + 1
+    const auxTotal = counter + bad + neutral
+    const auxAverage = Math.abs((counter + (bad * -1)) / auxTotal)
+    const auxPositive = (counter * 100) / auxTotal
     setGood(counter)
-
+    setTotal(counter + bad + neutral)
+    setAverage(auxAverage)
+    setPositive(auxPositive)
   }
+
   const hadlerSetNeutral = () => {
     const counter = neutral + 1
+    const auxTotal = counter + bad + good
+    const auxAverage = Math.abs((good + (bad * -1)) / auxTotal)
+    const auxPositive = (good * 100) / auxTotal
     setNeutral(counter)
-
+    setTotal(counter + good + bad)
+    setAverage(auxAverage)
+    setPositive(auxPositive)
   }
+
   const hadlerSetBad = () => {
     const counter = bad + 1
+    const auxTotal = counter + good + neutral
+    const auxAverage = Math.abs((good + (counter * -1)) / auxTotal)
+    const auxPositive = (good * 100) / auxTotal
     setBad(counter)
-
+    setTotal(counter + good + neutral)
+    setAverage(auxAverage)
+    setPositive(auxPositive)
   }
-
   return (
     <div>
       <Title text={'Give Feedback'} />
@@ -45,7 +68,7 @@ const App = () => {
       <Button onClick={hadlerSetNeutral} text={'Neutral'} />
       <Button onClick={hadlerSetBad} text={'bad'} />
       <Title text={'Statisics'} />
-      <Display goodValue={`Good ${good}`} neutralValue={`Neutral ${neutral}`} badValue={`Bad ${bad}`} />
+      <Display goodValue={`Good ${good}`} neutralValue={`Neutral ${neutral}`} badValue={`Bad ${bad}`} total={`All ${total}`} average={`Average ${average}`} positive={`Positive ${positive}%`} />
       <br />
     </div>
   )
