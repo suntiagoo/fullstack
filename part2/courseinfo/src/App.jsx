d3e7392b0e3cfea1166d291f3b1de0ea3d431de7
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2025-04-26 18:08:26 +0200

    2.3*: Course information step 8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -8,15 +8,16 @@ const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
 const Content = ({ courseParts }) => {
   return (
     <>
-      <Part name={courseParts[0].name} exercises={courseParts[0].exercises} />
-      <Part name={courseParts[1].name} exercises={courseParts[1].exercises} />
-      <Part name={courseParts[2].name} exercises={courseParts[2].exercises} />
+      {courseParts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
     </>
   )
 }
 
 const Total = ({ parts }) => {
-  return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  const total = parts.reduce((accumulator, item) => {
+    return accumulator += item.exercises;
+  }, 0)
+  return <p>Number of exercises {total}</p>
 }
 
 const Course = ({ course }) => {
