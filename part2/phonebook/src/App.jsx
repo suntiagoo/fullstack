ec434dfbcc784b9b434abf2a15db8f0f8de29ae2
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2025-04-29 11:14:57 +0200

    2.8: The Telephone Agenda Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from 'react'
 
-
 const Person = ({ people }) => {
   return (
     <ul>
-      {people.map(person => <li key={person.id}>{person.name}</li>)}
+      {people.map(person => <li key={person.id}> {person.name} {person.phone} </li>
+      )}
     </ul>
-
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([{
     name: 'Arto Hellas',
+    phone: '05154678945',
     id: 1
   }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
-  const hadleAddPerson = (event) => {
+  const hadleName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const hadlePhone = (event) => {
+    setNewPhone(event.target.value)
   }
 
   const addPerson = (event) => {
     event.preventDefault()
     const person = {
       name: newName,
+      phone: newPhone,
       id: persons.length + 1,
     }
     persons.find(person => person.name === newName) === undefined ? setPersons(persons.concat(person)) : alert(`${newName} is already added to phonebook`)
@@ -39,8 +45,13 @@ const App = () => {
         <legend><strong>Phonebook form</strong></legend>
         <form onSubmit={addPerson}>
           <label >
-            name:<strong>*</strong><input id='name' value={newName} onChange={hadleAddPerson} minLength={2} maxLength={20} placeholder='E.g Bob Muller' required />
+            name:<strong>*</strong><input id='name' value={newName} onChange={hadleName} minLength={2} maxLength={20} placeholder='E.g Bob Muller' required />
           </label>
+          <div>
+            <label>
+              Phone: <strong>*</strong> <input id='phone' value={newPhone} onChange={hadlePhone} minLength={7} maxLength={10} placeholder='E.g 0520599563' required />
+            </label>
+          </div>
           <div>
             <button type="submit">add</button>
           </div>
