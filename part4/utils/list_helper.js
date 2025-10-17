0782d452e233c44ad65c9583a6a86c8dc553d2e4
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2025-10-17 11:50:32 -0500

    4.4: Auxiliary Functions and Unit Tests, step 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,6 +4,29 @@ const dummy = (blogs) => {
     }
 }
 
+const isLikeInBlog = (blogs) => {
+    const even = (element) => !('likes' in element);
+    return blogs.some(even)
+}
+
+const filterArrayWithoutPropietyLikes = (blogs) => {
+    return blogs.filter(blog => ('likes' in blog))
+}
+
+const totalLikes = (blogs) => {
+    if (!Array.isArray(blogs)) {
+        return 'check if the value that you get is e array'
+    } else {
+        if (blogs.length === 0) {
+            return 0
+        } else
+            return blogs.filter(blog => ('likes' in blog)).reduce((acc, blog) => acc + blog.likes, 0)
+    }
+
+}
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
