1d75f6907f566f1bc427d98e7ba7ae5fd1db8221
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2025-10-27 11:35:13 -0500

    4.20*: Blog List Expansions, Step 8

--- part4/app.js
+++ part4/app.js
@@ -3,21 +3,21 @@ const express = require('express')
 //require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blog')
 const userRouter = require('./controllers/user')
 const loginRouter = require('./controllers/login')
-const middleware = require('./utils/middleware')
+
 
 
 app.use(express.json())
 app.use(cors())
 app.use(express.json())
-
+app.use(middleware.getTokenFrom)
 app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-
 module.exports = app
