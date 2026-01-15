a364cf455421040d70f6470c408fe4a98f339d97
Santiago Sandoval <191775772+suntiagoo@users.noreply.github.com>
2026-01-15 09:27:03 -0500

    8.6 & 8.7 GraphQL backend exercises for library app

--- part8/schema.js
+++ part8/schema.js
@@ -1,4 +1,5 @@
-const { authors, books } = require("./data");
+const { v1: uuid } = require("uuid");
+let { authors, books } = require("./data");
 
 const typeDefs = `#graphql
   enum Genres {
@@ -23,35 +24,125 @@ const typeDefs = `#graphql
   name: String!
   id: ID!
   born: Int
+  bookCount: Int
   }
+
+
+  type Mutation{
+  addBook(
+  title: String!
+  published: Int!
+  author: String!
+  genres: [String!]!): Book
+  editAuthor(
+  name: String!
+  born: Int!
+  bookCount: Int): Author
+  } 
 
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allAuthors: [Author!]!
     allbooks: [Book!]!
     allBooksWIthArg(author: String!):[Book]
-    allBooksAccordingToGenres(genres: Genres, author: String):[Book!]!
+    allBooksAccordingToGenresAndAuthor(genres: Genres, author: String):[Book!]!
   }
 `;
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length,
+    authorCount: () => books.length,
+    allAuthors: (root, arg) => {
+      const countBoogByAuthor = authors.map((author) => {
+        const totalBook = books.filter(
+          (book) => book.author === author.name
+        ).length;
+        return {
+          name: author.name,
+          id: author.id,
+          born: author.born,
+          bookCount: totalBook,
+        };
+      });
+      return countBoogByAuthor.sort((a, b) => b.bookCount - a.bookCount);
+
+      /*const counts = new Map();
+      books.forEach((book) => {
+        if (counts.has(book.author)) {
+          counts.set(book.author, counts.get(book.author) + 1);
+        } else counts.set(book.author, 1);
+      });
+
+      const authorWithNumberOfBook = [];
+      for (const count of counts) {
+        const author = books.find((book) => book.author === count[0]);
+        authorWithNumberOfBook.push({
+          name: author.author,
+          bookCount: count[1],
+        });
+      }
+      console.log(authorWithNumberOfBook);
+      return authorWithNumberOfBook;
+      */
+    },
     allbooks: () => books,
     allBooksWIthArg: (root, arg) => {
       if (!arg.author) {
         return books;
       }
-      books.filter((book) => book.author === arg.author);
+      return books.filter((book) => book.author === arg.author);
     },
-    allBooksAccordingToGenres: (root, arg) => {
+    allBooksAccordingToGenresAndAuthor: (root, arg) => {
       const bookGenre2 = (book) => {
         if (arg.author === book.author || book.genres.includes(arg.genres)) {
           return book;
         }
       };
       return arg.author || arg.genres ? books.filter(bookGenre2) : books;
+    },
+  },
+
+  /*Book: {
+    author: (root) => {
+      return {
+        name: root.name,
+        born: root.born,
+        id: root.id,
+      };
+    },
+  },*/
+  Mutation: {
+    addBook: (root, arg) => {
+      if (!books.find((book) => book.author === arg.author)) {
+        const author = {
+          name: arg.author,
+          born: null,
+          id: uuid(),
+          bookCount: 1,
+        };
+        authors = [...authors, author];
+        //authors = authors.concat(author);
+      }
+      // const author = authors.find(author=> author.name === arg.author)
+      //const totalBook = books.filter(book=> book.author === arg.author).length
+      //updateAuthor = {...author, bookCount: totalBook}
+      //authors.map(author=> author.name === arg.author? updateAuthor:author)
+      const book = { ...arg, id: uuid() };
+      books = [...books, book];
+      return book;
+    },
+    editAuthor: (root, arg) => {
+      const author = authors.find((author) => author.name === arg.name);
+      if (!author) {
+        return null;
+      }
+      const editAuthor = { ...author, born: arg.born };
+      authors = authors.map((author) =>
+        author.name === arg.name ? editAuthor : author
+      );
+      return editAuthor;
     },
   },
 };
